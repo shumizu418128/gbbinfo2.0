ce403c0d5dfb459c20247fb3727b63eeaedb0d9f
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-06-23 15:26:44 +0900

    htmlいらなかった

--- app/service-worker.js
+++ app/service-worker.js
@@ -5,7 +5,7 @@ const CACHE = "pwabuilder-offline-page";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const currentYear = new Date().getFullYear();
-const offlineFallbackPage = `/${currentYear}/top.html`;
+const offlineFallbackPage = `/${currentYear}/top`;
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
