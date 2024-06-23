98e773dd0f81b5c5595173489a41657a6cf21228
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-06-23 15:24:54 +0900

    replaceを忘れてた

--- app/service-worker.js
+++ app/service-worker.js
@@ -4,8 +4,8 @@ const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const currentYear = new Date().getFullYear();
+const offlineFallbackPage = `/${currentYear}/top.html`;
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
