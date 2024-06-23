c2e7aa82c86e5c30b353f02527602e80134c63da
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-06-23 15:18:48 +0900

    PWAbuilderにsw変えてもらった

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,53 +1,54 @@
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      // ネットワークからリクエストを取得する
-      fetch(event.request)
-        .then(function(response) {
-          // レスポンスが正常であれば、キャッシュを開き、そのレスポンスをキャッシュに保存する
-          if (response && response.status === 200) {
-            var responseToCache = response.clone();
-            caches.open('my-cache')
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-          }
-          return response; // レスポンスを返す
-        })
-        .catch(function() {
-          // ネットワークにアクセスできない場合はキャッシュを使用する
-          return caches.match(event.request);
-        })
-    );
-  });
+const CACHE = "pwabuilder-offline-page";
 
-// アクティベートイベントで古いキャッシュを削除
-self.addEventListener('activate', function(event) {
-  const cacheWhitelist = "my-cache";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener('sync', function(event) {
-  if (event.tag == 'update-content') {
-    event.waitUntil(
-      fetch('/latest-updates').then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        // ここで最新情報をキャッシュまたはページに更新する処理を行う
-        console.log('最新情報が更新されました', data);
-      }).catch(function(err) {
-        console.error('最新情報の取得に失敗しました', err);
-      })
-    );
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
   }
 });
