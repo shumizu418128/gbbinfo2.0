006132b510977288386558a32c939c070692b37d
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-07-01 19:15:19 +0900

    canonicalを設定

--- app/static/scripts/scroll.js
+++ app/static/scripts/scroll.js
@@ -1,15 +1,35 @@
+function smoothScroll(target, duration) {
+  var targetElement = document.querySelector(target);
+  var targetPosition = targetElement.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, targetPosition, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-      block: "start", // 'start', 'center', 'end', or 'nearest'
-    });
+    smoothScroll(this.getAttribute("href"), 500); // 500ms = 0.5秒
 
     // 少し遅延させてからスクロールする
     setTimeout(function () {
-      window.scrollBy(0, -120); // 100px上にスクロール
-    }, 500); // 500ms後にスクロール
+      window.scrollBy(0, -120); // 120px上にスクロール
+    }, 700); // 700ms後にスクロール
   });
 });
