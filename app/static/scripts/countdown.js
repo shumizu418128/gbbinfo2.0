8ab9873bb7ca44f54e9f955375bef427b0d919b3
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-09-24 12:12:03 +0900

    未発表ページに問い合わせ設置

--- app/static/scripts/countdown.js
+++ app/static/scripts/countdown.js
@@ -1,19 +1,19 @@
 // カウントダウンの終了日時
-var countDownDate = new Date("Nov 1, 2024 00:00:00 GMT+09:00").getTime();
+const countDownDate = new Date("Nov 1, 2024 00:00:00 GMT+09:00").getTime();
 
 function updateCountdown() {
   // 現在の日時をミリ秒で取得
-  var nowMillis = new Date().getTime();
+  const nowMillis = new Date().getTime();
 
   // 終了日時までの時間差
-  var distance = countDownDate - nowMillis;
+  const distance = countDownDate - nowMillis;
 
   // 時間、分、秒、ミリ秒に変換
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  var milliseconds = distance % 1000;  // ミリ秒を取得
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let milliseconds = distance % 1000;  // ミリ秒を取得
 
   // ミリ秒を3桁に揃える
   if (milliseconds < 100) {
