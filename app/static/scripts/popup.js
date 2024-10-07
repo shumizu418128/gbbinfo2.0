f9206f20846312fa40436703e827568e6aaf2c48
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-10-07 23:11:36 +0900

    年度切り替えボタンを設置

--- app/static/scripts/popup.js
+++ app/static/scripts/popup.js
@@ -1,13 +1,30 @@
 function showPopup() {
-    document.querySelector(".background-popup").style.display = "block";
-    document.querySelector(".popup").style.display = "block";
+  document.querySelector(".background-popup").style.display = "block";
+  document.querySelector(".popup").style.display = "block";
 }
 
 function closePopup() {
-    document.querySelector(".background-popup").style.display = "none";
-    document.querySelector(".popup").style.display = "none";
+  document.querySelector(".background-popup").style.display = "none";
+  document.querySelector(".popup").style.display = "none";
 }
 // ページが読み込まれたらポップアップを表示する
-window.onload = function() {
-    showPopup();
+window.onload = function () {
+  showPopup();
+};
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdown-content");
+  if (dropdownContent.style.display === "none") {
+    dropdownContent.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+  }
 }
+
+document.addEventListener('click', function (event) {
+  var dropdownContent = document.getElementById("dropdown-content");
+  var dropdownButton = document.getElementById("bottom-dropdown");
+  if (dropdownContent.style.display === 'block' && !dropdownButton.contains(event.target)) {
+    dropdownContent.style.display = 'none';
+  }
+});
