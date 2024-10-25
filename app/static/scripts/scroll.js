af92055afbdcb9b8349ad26b6a18572e2e4c1513
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-10-25 17:44:47 +0900

    プログレスバーを導入

--- app/static/scripts/scroll.js
+++ app/static/scripts/scroll.js
@@ -1,3 +1,41 @@
+// スクロールに応じてプログレスバーを更新するJavaScript
+window.onscroll = function() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    const progressTop = document.getElementById("progressTop");
+    const progressBottom = document.getElementById("progressBottom");
+
+    progressTop.style.width = scrolled + "%";
+    progressBottom.style.width = scrolled + "%";
+
+    if (scrolled === 100) {
+        progressTop.style.borderTopRightRadius = "0";
+        progressTop.style.borderBottomRightRadius = "0";
+        progressBottom.style.borderTopRightRadius = "0";
+        progressBottom.style.borderBottomRightRadius = "0";
+    }
+    else {
+        progressTop.style.borderTopRightRadius = "6px";
+        progressTop.style.borderBottomRightRadius = "6px";
+        progressBottom.style.borderTopRightRadius = "6px";
+        progressBottom.style.borderBottomRightRadius = "6px";
+    }
+
+    // 色を動的に変更（オレンジから白へ）
+    const rBottom = 240 + (15 * (scrolled / 100));
+    const gBottom = 99 + (156 * (scrolled / 100));
+    const bBottom = 47 + (208 * (scrolled / 100));
+
+    // 色を動的に変更（白からオレンジへ）
+    const rTop = 255 - (255 - 240) * (scrolled / 100);
+    const gTop = 255 - (255 - 99) * (scrolled / 100);
+    const bTop = 255 - (255 - 47) * (scrolled / 100);
+
+    progressTop.style.backgroundColor = `rgb(${rTop}, ${gTop}, ${bTop})`;
+    progressBottom.style.backgroundColor = `rgb(${rBottom}, ${gBottom}, ${bBottom})`;
+};
+
 // スムーズスクロールを実行する関数
 function smoothScroll(target, duration) {
     // 対象要素を取得
