954e9e8138ba2f56008e9d4008a346aee55b0e92
Haruki Shimizu <shumizu418128@gmail.com>
2025-02-09 12:35:12 +0900

    サマータイム追記

--- app/static/scripts/countdown.js
+++ app/static/scripts/countdown.js
@@ -1,12 +1,10 @@
 // カウントダウンの終了日時
-const countDownDate1 = new Date("Oct 31, 2025 0:00:00 GMT+09:00").getTime();
-const countDownDate2 = new Date("Oct 13, 2024 23:59:00 GMT+09:00").getTime();
+// GBB開催日時
+const countDownDate = new Date("Oct 31, 2025 14:00:00 GMT+09:00").getTime();
 
 function updateCountdown() {
     const nowMillis = new Date().getTime();
-
-    const distance1 = countDownDate1 - nowMillis;
-    // const distance2 = countDownDate2 - nowMillis;
+    const distance = countDownDate - nowMillis;
 
     // フォーマット関数
     function formatCountdown(distance) {
@@ -21,14 +19,7 @@ function updateCountdown() {
         return `${days} days ${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")} ${String(milliseconds).padStart(3, "0")}`;
     }
 
-    // GBB開催日
-    document.getElementById("countdown").innerHTML = formatCountdown(distance1);
-
-    // チケット販売カウントダウン（オプション）
-    // const ticketCountdownElement = document.getElementById("ticketDeadCountdown");
-    // if (ticketCountdownElement) {
-    //     ticketCountdownElement.innerHTML = formatCountdown(distance2);
-    // }
+    document.getElementById("countdown").innerHTML = formatCountdown(distance);
     requestAnimationFrame(updateCountdown);
 }
 requestAnimationFrame(updateCountdown);
